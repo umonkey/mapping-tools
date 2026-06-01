4903769f0ea6aa15777242245229eab8ca51edb4
Justin Forest <hex@umonkey.net>
2026-06-01 13:45:23 +0400

    feat(upload): upload variables

--- upload_to_mapillary.sh
+++ upload_to_mapillary.sh
@@ -3,15 +3,20 @@
 # uv run python3 -m app src/stretched.mp4 src/track.gpx dst --distance 10.0
 
 export MAPILLARY_TOOLS_MAX_SEQUENCE_PIXELS=100000000000
-export MAPILLARY_TOOLS_MAX_SEQUENCE_LENGTH=10000
+export MAPILLARY_TOOLS_MAX_SEQUENCE_LENGTH=99999
 
-if [ ! -d dst ]; then
-    echo "This script uploads JPEG files from ./dst to Mapillary."
+if [ -z "$1" ]; then
+    echo "Usage: $0 image_folder_path"
+    exit 1
+fi
+
+if [ ! -d "$1" ]; then
+    echo "This script uploads JPEG files from a folder to Mapillary."
     echo "Please run the app first to generate the files."
     exit 1
 fi
 
-mapillary_tools process_and_upload ./dst \
+mapillary_tools process_and_upload "$1" \
   --organization_key "3351990278289209" \
   --cutoff_distance 999999 \
   --cutoff_time 999999 \
